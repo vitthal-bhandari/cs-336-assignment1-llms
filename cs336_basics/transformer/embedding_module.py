873de917ac2d797213e67394631446b583eea9f1
vitthal-bhandari <vitthalbhandari98@gmail.com>
2026-02-09 11:27:05 -0800

    send device and dtype to weight matrices

--- cs336_basics/transformer/embedding_module.py
+++ cs336_basics/transformer/embedding_module.py
@@ -4,11 +4,10 @@ import torch.nn as nn
 class EmbeddingModule(nn.Module):
     def __init__(self, num_embeddings: int, embedding_dim: int, device = None, dtype = None):
         super().__init__()
+        factory_kwargs = {"device": device, "dtype": dtype}
         self.num_embeddings = num_embeddings
         self.embedding_dim = embedding_dim
-        self.device = device
-        self.dtype = dtype
-        self.E = nn.Parameter(torch.randn(num_embeddings, embedding_dim))
+        self.E = nn.Parameter(torch.empty(num_embeddings, embedding_dim, **factory_kwargs))
         nn.init.trunc_normal_(self.E, mean=0, std=1, a=-3, b=3)
 
     def forward(self, token_ids: torch.Tensor) -> torch.Tensor:
--- cs336_basics/transformer/rmsnorm_module.py
+++ cs336_basics/transformer/rmsnorm_module.py
@@ -5,11 +5,10 @@ import torch.nn as nn
 class RMSNormModule(nn.Module):
     def __init__(self, d_model: int, eps: float = 1e-5, device = None, dtype = None):
         super().__init__()
+        factory_kwargs = {"device": device, "dtype": dtype}
         self.d_model = d_model
         self.eps = eps
-        self.device = device
-        self.dtype = dtype
-        self.W = nn.Parameter(torch.ones(d_model))
+        self.W = nn.Parameter(torch.ones(d_model, **factory_kwargs))
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         in_dtype = x.dtype
